f95bc1ac2956ac762a24999d277bdbbfc92bd676
Cydlet <camacho.aero@outlook.com>
2017-02-04 16:07:55 +0000

    working signup UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root'page#home'
   get 'about', to: 'page#about'
   resources :articles
-
+  
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
